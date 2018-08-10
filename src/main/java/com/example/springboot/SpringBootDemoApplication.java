8d0b6e75b0d6cf11e34febc1a5a89fb32b1dc53d
milolovewenlly <lyy520lyy>
2018-08-10 17:55:56 +0800

    init

---
+++ src/main/java/com/example/springboot/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.springboot.dao")
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemo1Application.class, args);
+    }
+}
